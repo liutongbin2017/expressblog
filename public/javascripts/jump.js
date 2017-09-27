8fb006d40c8421811f18c191ac3a594db1c4d8a4
liutongbin <liutongbin@gmail.com>
2017-09-27 15:34:38 +0800

    new

--- public/javascripts/jump.js
+++ public/javascripts/jump.js
@@ -3,9 +3,39 @@
  */
 ;(function ($) {
     $(function(){
-        $("#jump").click(function () {
-            var username = $("#username").val();
-            var password = $("#password").val();
+        $("#inputSuccess1").blur(function () {
+            var text_value = $(this).val(),
+                $par = $(this).parent();
+            console.log($par);
+            if(text_value == ""){
+                $par.addClass("has-error");
+
+            }else{
+                $par.removeClass("has-error").addClass("has-success");
+            }
+        }).focus(function() {
+            var $this = $(this),
+                $par = $this.parent();
+
+            $par.removeClass("has-error").removeClass("has-success");
+        });
+        $("#inputSuccess2").blur(function () {
+            var text_value = $(this).val();
+            var $par =$(this).parent();
+            if(text_value == ""){
+                $par.addClass("has-error");
+
+            }else{
+                $par.removeClass("has-error").addClass("has-success");
+            }
+        }).focus(function(){
+            var $this = $(this);
+            $par = $this.parent();
+            $par.removeClass("has-error").removeClass("has-success");
+        })
+        $(".btn").click(function () {
+            var username = $("#inputSuccess1").val();
+            var password = $("#inputSuccess2").val();
             if($.trim(username)==""){
                 alert("name is error!");
                 return false;
